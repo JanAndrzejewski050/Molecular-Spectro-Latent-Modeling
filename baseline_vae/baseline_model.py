ae95b47addc16a4d8bc558e282e811a72f250a11
nerkulec <nerkulec@github.com>
2025-12-01 13:53:02 +0100

    Fix imports

--- baseline_vae/baseline_model.py
+++ baseline_vae/baseline_model.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, TensorDataset
 import torch.functional as F
+import numpy as np
 
 class BaselineVAE(nn.Module):
     def __init__(self, vocab_size, max_len, embed_size=64, hidden_size=128, latent_size=64):
