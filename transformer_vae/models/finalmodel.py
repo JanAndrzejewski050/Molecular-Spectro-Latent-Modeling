bb1f9cb3722f6ba5a4c1061e1adc132c868cafe1
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2026-02-01 12:45:46 +0100

    final NAT model

--- transformer_vae/models/finalmodel.py
+++ transformer_vae/models/finalmodel.py
@@ -64,19 +64,14 @@ class MultiHeadAttention(nn.Module):
         attn_logits = torch.einsum('baih,bajh->baij', Q, K)    # [B, A, T, H] @ [B, A, H, T] = [B, A, T, T]
 
         if pad_mask is not None:
-            valid = pad_mask[:, None, None, :]          # keys [B, 1, 1, T]
-            query_valid = pad_mask[:, None, :, None]    # queries [B, 1, T, 1]
-            mask = ~(valid & query_valid)  # [B, 1, T, T]
-            attn_logits = attn_logits.masked_fill(mask, float('-inf'))
+            key_mask = pad_mask[:, None, None, :]  # [B,1,1,T_k]
+            attn_logits = attn_logits.masked_fill(~key_mask, float('-inf'))
+
+        attn = F.softmax(attn_logits / math.sqrt(self.d), dim=-1)
 
         if pad_mask is not None:
-            attn = torch.where(
-                query_valid,
-                F.softmax(attn_logits / math.sqrt(self.d), dim=-1),
-                torch.zeros_like(attn_logits)
-            )
-        else:
-            attn = F.softmax(attn_logits / math.sqrt(self.d), dim=-1)
+            query_mask = pad_mask[:, None, :, None]  # [B,1,T_q,1]
+            attn = attn * query_mask.float()
 
         h = torch.einsum('baij,bajh->baih',attn, V)  # [B, A, T, H]
         h = h.view(B, T_q, H)  # [B, T, A*H]
@@ -86,6 +81,50 @@ class MultiHeadAttention(nn.Module):
         h = self.norm2(h + self.ff(h))
         return h
     
+def phi(x):
+    return F.elu(x) + 1
+
+class LinearMultiHeadAttention(nn.Module):
+    def __init__(self, hidden_size, num_heads):
+        super().__init__()
+        assert hidden_size % num_heads == 0
+        
+        self.num_heads = num_heads
+        self.d = hidden_size // num_heads
+
+        self.W_q = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.W_k = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.W_v = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.W_o = nn.Linear(hidden_size, hidden_size, bias=False)
+
+    def forward(self, q, k, v, mask=None):
+        B, Tq, H = q.shape
+        _, Tk, _ = k.shape
+
+        Q = self.W_q(q)
+        K = self.W_k(k)
+        V = self.W_v(v)
+        Q = Q.view(B, Tq, self.num_heads, self.d).transpose(1, 2)
+        K = K.view(B, Tk, self.num_heads, self.d).transpose(1, 2)
+        V = V.view(B, Tk, self.num_heads, self.d).transpose(1, 2)
+        Q = phi(Q)
+        K = phi(K)
+
+        if mask is not None:
+            mask = mask[:, None, :, None]  # [B, 1, T, 1]
+            #Q = Q * mask
+            K = K * mask
+            V = V * mask
+
+        K_sum = K.sum(dim=2)                      # [B, heads, d]
+        KV = torch.einsum('bhtd,bhtv->bhdv', K, V)  # [B, heads, d, d]
+        Z = 1 / (torch.einsum('bhtd,bhd->bht', Q, K_sum) + 1e-6)
+
+        out = torch.einsum('bhtd,bhdv,bht->bhtv', Q, KV, Z)
+        out = out.transpose(1, 2).contiguous().view(B, Tq, H)
+        out = self.W_o(out)
+        return out
+
 class MultiSlotPooling(nn.Module):
     def __init__(self, hidden_size, num_slots):
         super().__init__()
@@ -100,6 +139,29 @@ class MultiSlotPooling(nn.Module):
         slots = torch.einsum("bkt,btd->bkd", attn, h)
         return slots
 
+class LinearSlotPooling(nn.Module):
+    def __init__(self, hidden_size, num_slots):
+        super().__init__()
+        self.queries = nn.Parameter(torch.randn(num_slots, hidden_size))
+        self.norm = nn.LayerNorm(hidden_size)
+
+    def phi(self, x):
+        return F.elu(x) + 1.0
+
+    def forward(self, h, mask):
+        Q = self.phi(self.queries)                 # [K, H]
+        H = self.phi(h)                            # [B, T, H]
+
+        w = torch.einsum("kh,bth->bkt", Q, H)
+        w = w * mask[:, None, :]                   # mask padding
+
+        slots = torch.einsum("bkt,bth->bkh", w, h)
+
+        Z = w.sum(dim=-1, keepdim=True) + 1e-6
+        slots = slots / Z
+
+        return self.norm(slots)
+
 class VaeTransformer(nn.Module):
     def __init__(self, vocab_size, hidden_size, latent_size, max_len, attn_heads=8, num_slots=8, layers=1, mask_token_id=0, seed_size=5):
         super().__init__()
@@ -110,8 +172,9 @@ class VaeTransformer(nn.Module):
         
         # Encoder
         self.encoder_blocks = nn.ModuleList([MultiHeadAttention(hidden_size, attn_heads) for _ in range(layers)])
-        self.pool = MultiSlotPooling(hidden_size, num_slots=num_slots)
-        
+        #self.pool = MultiSlotPooling(hidden_size, num_slots=num_slots)
+        self.pool = LinearSlotPooling(hidden_size, num_slots=num_slots)
+
         # VAE heads
         self.fc_mu = nn.Linear(num_slots * hidden_size, latent_size)
         self.fc_logvar = nn.Linear(num_slots * hidden_size, latent_size)
@@ -126,7 +189,8 @@ class VaeTransformer(nn.Module):
             nn.GELU(),
             nn.Linear(latent_size // 2, 1)
         )
-        self.cross_block = nn.MultiheadAttention(hidden_size, attn_heads, batch_first=True)
+        #self.cross_block = nn.MultiheadAttention(hidden_size, attn_heads, batch_first=True)
+        self.cross_block = LinearMultiHeadAttention(hidden_size, attn_heads)
         self.decoder_blocks = nn.ModuleList([MultiHeadAttention(hidden_size, num_heads=attn_heads) for _ in range(layers)])
         
         # Output head
@@ -144,13 +208,16 @@ class VaeTransformer(nn.Module):
         B, _ = x.shape
         h = self.embedding(x)    # [B, T, H]
         pos_encoding = self.pos_encoder(x)
-        h += pos_encoding
+        h_pos = h + pos_encoding
         mask = (x != 0)
         
         for block in self.encoder_blocks:
-            h = block(h, h, h, pad_mask=mask)
-        
+            h = block(h_pos, h_pos, h, pad_mask=mask)
+        h = h * mask[:, :, None]
+
         slots = self.pool(h, mask)
+        slots = slots - slots.mean(dim=1, keepdim=True)
+        slots = F.layer_norm(slots, slots.shape[-1:])
         z_input = slots.reshape(B, -1)
         mu = self.fc_mu(z_input)
         logvar = self.fc_logvar(z_input)
@@ -167,45 +234,32 @@ class VaeTransformer(nn.Module):
     def decode(self, z, x=None, mode='eval', sos_id=1):
         B, _ = z.shape
         pred_len = F.softplus(self.fc_lenght(z.detach())).squeeze(-1)
+        pred_len_i = torch.round(pred_len).long()
+        pred_len_i = torch.clamp(pred_len_i, min=1)
+        
         z = self.fc_z2h(z)
         slots = z.view(B, self.num_slots, self.hidden_size)
-        max_len = int(torch.round(pred_len.max()).item())
+        slots = slots - slots.mean(dim=1, keepdim=True)
+        slots = F.layer_norm(slots, slots.shape[-1:])
+        max_len_batch = int(pred_len.max().item())
+        t = torch.arange(max_len_batch, device=z.device)[None, :] 
 
-        if mode == 'train': 
-            mask = (x != 0)
-            h = torch.zeros(B, max_len, self.hidden_size, device=z.device)
-            h[:, 0, :] = self.embedding.weight[sos_id]
-            h += self.pos_encoder(h)
-            
-
-            h, _ = self.cross_block(h, slots, slots)    
-                      
-            for block in self.decoder_blocks:
-                h = block(h, h, h)
-            
-            logits = self.fc_output(h)
-            
-            return logits, pred_len
+        pos_q = self.pos_encoder(torch.empty(B, max_len_batch, self.hidden_size, device=z.device))
+        pos_q = pos_q.expand(B, -1, -1).detach().clone()
+        pos_q[:, 0, :] = self.embedding.weight[sos_id]
         
-        else:              
-            outputs = []
-            for i in range(B):
-                Li = max(1, int(torch.round(pred_len[i]).item())) 
-                slots_i = slots[i:i+1]
-                h = torch.zeros(1, Li, self.hidden_size, device=z.device)  
-                h[:, 0, :] = self.embedding.weight[sos_id] 
-                h += self.pos_encoder(h)     
+        h = self.cross_block(pos_q, slots, slots)  
+        h = F.layer_norm(h, h.shape[-1:]) 
 
-                h, _ = self.cross_block(h, slots_i, slots_i)
+        dec_mask = t < pred_len_i[:, None]
 
-                for block in self.decoder_blocks:
-                    h = block(h, h, h)
-                
-                logits = self.fc_output(h)         
-                outputs.append(logits.squeeze(0))  
-            
-            return outputs, pred_len
-
+        for block in self.decoder_blocks:
+            h = block(h, h, h, pad_mask=dec_mask)
+        
+        logits = self.fc_output(h)
+        
+        return logits, pred_len
+        
     def forward(self, x, mode='eval'):  
         mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
@@ -216,13 +270,9 @@ class VaeTransformer(nn.Module):
         
         return logits, mu, logvar, pred_lenght        
 
-
-def vae_loss(logits, x, mu, logvar, pred_len,
-             beta=0.01, alpha=0.1, pad_id=0):
-
+def vae_loss(logits, x, mu, logvar, pred_len, beta=0.01, alpha=0.1, pad_id=0):
     B, T_pred, V = logits.shape
     T_x = x.size(1)
-
     true_len = (x != pad_id).sum(dim=-1)                # [B]
     pred_len_i = torch.round(pred_len).long().clamp(min=1)  # [B]
 
@@ -242,16 +292,8 @@ def vae_loss(logits, x, mu, logvar, pred_len,
     targets = x[:, :min_T].reshape(-1)
     mask = mask.reshape(-1)
 
-    rec_loss = F.cross_entropy(
-        logits[mask],
-        targets[mask]
-    )
-
-    kl_loss = -0.5 * torch.mean(
-        1 + logvar - mu.pow(2) - logvar.exp()
-    )
-
+    rec_loss = F.cross_entropy(logits[mask],targets[mask])
+    kl_loss = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
     len_loss = F.mse_loss(pred_len, true_len.float())
-
     loss = rec_loss + beta * kl_loss + alpha * len_loss
     return loss, rec_loss, kl_loss, len_loss
